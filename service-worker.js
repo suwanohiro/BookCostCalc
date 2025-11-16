a985ce6ab4b13b3e14f76e637e45ef4038b75bd2
suwanohiro <supermariomaker1110@gmail.com>
2025-11-16 14:51:58 +0900

    PWA対応

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'book-cost-calc-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/css/style.css',
+    '/assets/js/script.js',
+    '/manifest.json',
+    '/assets/icons/icon-192.png',
+    '/assets/icons/icon-512.png'
+];
+
+// インストール時にキャッシュを作成
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// フェッチ時にキャッシュから返す
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // キャッシュがあればそれを返す、なければネットワークから取得
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// アクティベート時に古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
